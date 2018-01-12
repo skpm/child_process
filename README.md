0add992994b945c2d30170e65b8c3dd0d882fd11
mathieudutour <mathieu@dutour.me>
2018-01-12 20:45:20 +0100

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,3 @@
+# `child_process` for Sketch
+
+All the sync [nodejs child_process](https://nodejs.org/api/child_process.html) API is available.
---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.exec = require('./lib/exec')
+module.exports.execFile = require('./lib/execFile')
+module.exports.spawn = require('./lib/spawn')
