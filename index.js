512c9846e59dd961b2a704b6f297dfe58bbf9c14
mathieudutour <mathieu@dutour.me>
2018-02-21 12:27:23 +0200

    fix spawn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.exec = require('./lib/exec')
 module.exports.execFile = require('./lib/execFile')
 module.exports.spawn = require('./lib/spawn')
+module.exports.spawnSync = require('./lib/spawnSync')
